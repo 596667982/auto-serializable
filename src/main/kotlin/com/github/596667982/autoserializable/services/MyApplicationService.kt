88b305ab6c050849fc1fa0f24cb6dd550b10c92a
GitHub Action <action@github.com>
2023-02-09 04:48:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/596667982/autoserializable/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/596667982/autoserializable/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.596667982.autoserializable.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.596667982.autoserializable.MyBundle
 
 class MyApplicationService {
 
